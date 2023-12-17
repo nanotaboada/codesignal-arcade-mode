6a4ba378ed11a08bd8f714d9d1fea41e7f053a68
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2023-12-17 17:10:23 -0300

    feat: add Level 5 - Island of Knowledge (22, 23 and 24)

---
+++ Intro/Level 5 - Island of Knowledge/22 - avoidObstacles/AvoidObstacles.cs
@@ -0,0 +1,49 @@
+/*  
+    Problem
+    --------------------------------------------------------------------------------
+    You are given an array of integers representing coordinates of obstacles
+    situated on a straight line.
+    
+    Assume that you are jumping from the point with coordinate 0 to the right.
+    You are allowed only to make jumps of the same length represented by some
+    integer.
+    
+    Find the minimal length of the jump enough to avoid all the obstacles.
+    
+    Example
+    
+    For inputArray = [5, 3, 6, 7, 9], the output should be
+    solution(inputArray) = 4.
+    
+    Check out the image below for better understanding:
+
+              jump = 4
+         /-----------------\ /-----------------\ /-------------                  
+        |              X    |    X    X    X    |    X
+    ----|----|----|----|----|----|----|----|----|----|----|----
+        0    1    2    3    4    5    6    7    8    9    10
+*/
+
+/*  
+    Solution
+    --------------------------------------------------------------------------------
+*/
+
+int solution(int[] inputArray)
+{
+    Array.Sort(inputArray);
+
+    var minimalLength = 1;
+
+    // The modulus operation checks if the obstacle's point is divisible by
+    // the current jump length. If any obstacle is found at a point
+    // divisible by the jump length, the current jump length is invalid.
+    while (inputArray.Any(obstacle => obstacle % minimalLength == 0))
+    {
+        // Continue increasing the minimum jump length until a valid length
+        // is found.
+        minimalLength++;
+    }
+
+    return minimalLength;
+}
---
+++ Intro/Level 5 - Island of Knowledge/24 - Minesweeper/Minesweeper.cs
@@ -0,0 +1,100 @@
+/*  
+    Problem
+    --------------------------------------------------------------------------------
+    In the popular Minesweeper game you have a board with some mines and those
+    cells that don't contain a mine have a number in it that indicates the total
+    number of mines in the neighboring cells. Starting off with some arrangement
+    of mines we want to create a Minesweeper game setup.
+    
+    Example
+    
+    For matrix =
+        [
+            [true, false, false],
+            [false, true, false],
+            [false, false, false]
+        ]
+
+    the output should be
+    solution(matrix) =
+        [
+            [1, 2, 1],
+            [2, 1, 1],
+            [1, 1, 1]
+        ]
+*/
+
+/*  
+    Solution
+    --------------------------------------------------------------------------------
+    To better illustrate the concept of neighboring cells, let's use the analogy
+    of cardinal points:
+    
+    ----------------------------------------------
+    | Cardinal points  | Offsets (x, y)          |
+    ----------------------------------------------
+    |  NW    N    NE   | (-1,-1) (-1, 0) (-1,+1) |
+    |    \   |   /     |                         |
+    |     \  |  /      |                         |
+    |  W --  ?  -- E   | ( 0,-1) ( 0, 0) ( 0,+1) |
+    |      / |  \      |                         |
+    |     /  |   \     |                         |
+    |  SW    S    SE   | (+1,-1) (+1, 0) (+1,+1) |
+    ----------------------------------------------
+
+    --------------------------------------
+    | Cardinal points  | Offsets [x, y]  |
+    --------------------------------------
+    | North West       | x = -1, y = -1  |
+    | North            | x = -1, y =  0  |
+    | North East       | x = -1, y =  1  |
+    | West             | x =  0, y = -1  |
+    | East             | x =  0, y =  1  |
+    | South West       | x = 1,  y = -1  |
+    | South            | x = 1,  y = 0   |
+    | South East       | x = 1,  y = 1   |
+    --------------------------------------
+*/
+
+int[][] solution(bool[][] matrix)
+{
+    var rows = matrix.Length;
+    var columns = matrix[0].Length;
+    var board = new int[rows][];
+    
+    for (var row = 0; row < rows; row++)
+    {
+        board[row] = new int[columns];
+
+        for (var column = 0; column < columns; column++)
+        {
+            board[row][column] = GetNeighboringMines(matrix, row, column);
+        }
+    }
+
+    return board;
+}
+
+static int GetNeighboringMines(bool[][] matrix, int row, int column)
+{
+    var mines = 0;
+
+    var xOffsets = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
+    var yOffsets = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+    // Scan for all 8 cardinal points
+    for (var cell = 0; cell < 8; cell++)
+    {
+        var x = row + xOffsets[cell];
+        var y = column + yOffsets[cell];
+
+        if (x >= 0 && x < matrix.Length
+            && y >= 0 && y < matrix[0].Length
+            && matrix[x][y]) // matrix[x][y] == "true"
+        {
+            mines++;
+        }
+    }
+
+    return mines;
+}
