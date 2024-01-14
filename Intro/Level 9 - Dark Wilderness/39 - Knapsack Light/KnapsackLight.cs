7e1821fc03e9e2c4eb52377145d2b06afcd12b2a
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-01-14 17:50:28 -0300

    feat: add Level 9 - Dark Wilderness (38 and 39)

---
+++ Intro/Level 9 - Dark Wilderness/39 - Knapsack Light/KnapsackLight.cs
@@ -0,0 +1,74 @@
+/*  
+    Problem
+    --------------------------------------------------------------------------------
+    You found two items in a treasure chest! The first item weighs weight1
+    and is worth value1, and the second item weighs weight2 and is worth value2.
+    What is the total maximum value of the items you can take with you, assuming
+    that your max weight capacity is maxW and you can't come back for the items
+    later?
+
+    Note that there are only two items and you can't bring more than one item of
+    each type, i.e. you can't take two first items or two second items.
+
+    Example
+
+    For value1 = 10, weight1 = 5, value2 = 6, weight2 = 4, and maxW = 8,
+    the output should be
+    solution(value1, weight1, value2, weight2, maxW) = 10.
+
+    You can only carry the first item.
+
+    For value1 = 10, weight1 = 5, value2 = 6, weight2 = 4, and maxW = 9,
+    the output should be
+    solution(value1, weight1, value2, weight2, maxW) = 16.
+
+    You're strong enough to take both of the items with you.
+    
+    For value1 = 5, weight1 = 3, value2 = 7, weight2 = 4, and maxW = 6,
+    the output should be
+    solution(value1, weight1, value2, weight2, maxW) = 7.
+    
+    You can't take both items, but you can take any of them.
+
+    Guaranteed constraints:
+    2 ≤ value1 ≤ 20.
+    2 ≤ weight1 ≤ 10.
+    2 ≤ value2 ≤ 20.
+    2 ≤ weight2 ≤ 10.
+    1 ≤ maxW ≤ 20.
+*/
+
+/*  
+    Solution
+    --------------------------------------------------------------------------------
+*/
+
+int solution(int value1, int weight1, int value2, int weight2, int maxW)
+{
+    // You cannot carry any item.
+    // (both exceed your max weight capacity).
+    if (weight1 > maxW && weight2 > maxW)
+    {
+        return 0;
+    }
+    // You're strong enough to take both of the items with you.
+    else if (weight1 + weight2 <= maxW)
+    {
+        return value1 + value2;
+    }
+    // You can only carry the first item.
+    else if (weight1 <= maxW && weight2 > maxW)
+    {
+        return value1;
+    }
+    // You can only carry the second item.
+    else if (weight2 <= maxW && weight1 > maxW)
+    {
+        return value2;
+    }
+    // You can't take both items, but you can take any of them.
+    else
+    {
+        return Math.Max(value1, value2);
+    }
+}
